9542325651f78d252202c302ab614459837d3419
ivan-guerra <ivan.eduardo.guerra@gmail.com>
2024-05-26 03:32:52 -0400

    Add PID 1 solution.

--- src/solns/soln_1/soln_1.cpp
+++ src/solns/soln_1/soln_1.cpp
@@ -2,14 +2,29 @@
 
 #include <iostream>
 
+#include "common/conversions.h"
 #include "common/types.h"
 
 namespace euler {
 
 types::SolnRetCode Soln1(const types::SolnFuncArgs& args) {
-  for (const auto& arg : args) {
-    std::cout << arg << std::endl;
+  if (args.size() != 1) {
+    return types::SolnRetCode::kInvalidArgCount;
   }
+
+  auto limit = euler::conv::StrToULong(args[0]);
+  if (!limit.has_value()) {
+    return types::SolnRetCode::kCannotConvertStrToUInt;
+  }
+
+  unsigned long accumulator = 0;
+  for (unsigned long i = 0; i < limit.value(); ++i) {
+    if ((0 == i % 3) || (0 == i % 5)) {
+      accumulator += i;
+    }
+  }
+  std::cout << accumulator << std::endl;
+
   return types::SolnRetCode::kSuccess;
 }
 
