d7cf7f86dbcdffe1375c2833f28c0924935d5da2
ivan-guerra <ivan.eduardo.guerra@gmail.com>
2024-05-26 03:32:19 -0400

    Link the soln lib and common lib.

--- cmake/define_soln_test_target.cmake
+++ cmake/define_soln_test_target.cmake
@@ -6,7 +6,7 @@ function(define_soln_test_target id)
 
   add_executable(${testname} ${testname}.cpp)
 
-  target_link_libraries(${testname} PRIVATE gtest_main)
+  target_link_libraries(${testname} PRIVATE gtest_main common soln_${id})
 
   gtest_discover_tests(${testname} WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/test)
 
